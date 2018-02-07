7b6366a1ce0a4a7327aeff6ed16a96eda453515f
sermshar <stephen.ermshar@wallawalla.edu>
2018-02-06 18:01:17 -0800

    adds page component  and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 import { RequestService } from './RequestService/requests';
 import { AppComponent } from './app.component';
+import { PageComponent } from './routes/routes';
+import { ProfileSmComponent, NavBarComponent, UserBubbleComponent, SubNavBarComponent, MobileNavComponent, UnescapePipe } from './shared/shared';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    ProfileSmComponent, 
+    NavBarComponent, 
+    UserBubbleComponent, 
+    SubNavBarComponent, 
+    MobileNavComponent, 
+    UnescapePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        "path": 'page',
+        component: PageComponent
+      }
+    ])
+
   ],
   providers: [
     RequestService
---
+++ src/app/shared/shared.ts
@@ -0,0 +1,7 @@
+export * from './profile-sm/profile-sm.component';
+export * from './profile.model';
+export * from './nav-bar/nav-bar.component';
+export * from './sub-nav-bar/sub-nav-bar.component';
+export * from './user-bubble/user-bubble.component';
+export * from './mobile-nav/mobile-nav.component';
+export * from './unescape';
