a814573abf0f476dc02026b805c494731b57c33f
sermshar <stephen.ermshar@wallawalla.edu>
2018-02-12 19:33:49 -0800

    adds javascript html insertion and url parameters

--- src/app/routes/page/page.component.ts
+++ src/app/routes/page/page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RequestService } from '../../RequestService/requests';
 
 @Component({
   selector: 'app-page',
@@ -9,23 +11,34 @@ export class PageComponent implements OnInit {
 
   page: any;
 
-  constructor() { }
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.page = { 
-        'url': "test",
-        'title': "Title",
-        'content': '<h1>test!</h1> <script>console.log("Hello there!");</script><a style="color:green;" href="https://google.com">the google</a>',
-        'owner': "ryan.rabello",
-        'editors': [{"username": "ryan.rabello"}, {"username": "stephen.ermshar"}],
-        'is_visible': true,
-        'created': "some valid date string",
-        'tags': [{"tag": "cool"},{"tag": "blue"}],
-        'category': "collegian",
-        'department': "collegian",
-        'current': true
-      }; }, 1000);
+  constructor( private request: RequestService, private route: ActivatedRoute ) {
+    this.route.params.subscribe( params => {
+      this.request.get( ('/pages/' + params.pageURL), (data) => this.page = data, null );
+      console.log(('/pages/' + params.pageURL));
+    });
   }
 
+  ngOnInit() {
+    // ----- TEST DATA -----
+    // setTimeout(() => {
+    //   this.page = { 
+    //     'url': "test",
+    //     'title': "Title",
+    //     'content': '<h1>test!</h1> <script>console.log("Hello there!");</script><a style="color:green;" href="https://google.com">the google</a>',
+    //     'owner': "ryan.rabello",
+    //     'editors': [{"username": "ryan.rabello"}, {"username": "stephen.ermshar"}],
+    //     'is_visible': true,
+    //     'created': "some valid date string",
+    //     'tags': [{"tag": "cool"},{"tag": "blue"}],
+    //     'category': "collegian",
+    //     'department': "collegian",
+    //     'current': true
+    //   }; }, 1000);
+  }
+
+  loadContent() {
+    document.getElementById('content').innerHTML = this.page.content;
+    return null;
+  }
+  
 }
