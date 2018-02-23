6508ad94a99b507978777d7f3bcdb4ac5fbddda2
sermshar <stephen.ermshar@wallawalla.edu>
2018-02-22 16:35:31 -0800

    fixes conflicts I missed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { RequestService } from './RequestService/requests';
 import { AppComponent } from './app.component';
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,8 +1,5 @@
 export * from './page/page.component';
-<<<<<<< HEAD
 export * from './admin/admin.component';
 export * from './admin-create/admin-create.component';
-=======
 export * from './edit/edit.component';
 export * from './revisions/revisions.component';
->>>>>>> develop
