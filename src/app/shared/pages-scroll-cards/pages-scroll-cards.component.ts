57ad91129852f789c00a66d12f9eec49c75a3dcf
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-03-02 11:18:50 -0800

    Added page-card component and refactored pages-scroll-cards component to incorporate it.

--- src/app/shared/pages-scroll-cards/pages-scroll-cards.component.ts
+++ src/app/shared/pages-scroll-cards/pages-scroll-cards.component.ts
@@ -9,17 +9,4 @@ import { Component, Input } from '@angular/core';
 export class PagesScrollCardsComponent {
   @Input() requestData: any;
   @Input() showMeta: boolean;
-
-  getAuthor(page) {
-    let author = page['author'].replace(/\./gi, ' ');
-    // https://stackoverflow.com/questions/4878756/how-to-capitalize-first-letter-of-each-word-like-a-2-word-city
-    return author.replace(/\w\S*/g, function(txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-  }
-
-  getDateCreated(page) {
-    let date = new Date(page['created']);
-    return date.toLocaleString().replace(/,.*/, '');
-  }
 }
