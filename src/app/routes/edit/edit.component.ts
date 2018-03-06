a5c967a35d52c7ea6fc0a4655a6aa35872529e6f
Ryan Rabello <ryan.s.rabello@gmail.com>
2018-03-05 17:52:36 -0800

    Updated Editors input so that it auto completes the usename

--- src/app/routes/edit/edit.component.ts
+++ src/app/routes/edit/edit.component.ts
@@ -12,7 +12,9 @@ export class EditComponent implements OnInit {
   page: any = {};
   departments: string[] = [];
   categories: string[] = [];
-  tags: string[] = [];     // TODO: get a list of tags from the server.
+  allTags: string[] = [];     // TODO: get a list of tags from the server.
+  editors: string[] = [];
+  allUsers: any[] = [];
 
   public options: Object = {
     imageUploadURL: environment.SERVER_URL + '/pages/media/upload_image',
@@ -21,14 +23,26 @@ export class EditComponent implements OnInit {
 
   constructor(private requestService: RequestService, private route: ActivatedRoute, private router: Router) {
     this.route.params.subscribe( params => {
-      this.requestService.get( ('/pages/' + params.pageURL), (data) => this.page = data, null );
+      this.requestService.get( ('/pages/admin/' + params.pageURL), (data) => {
+        this.page = data;
+        this.editors = data.editors;
+      }, null );
     });
-    this.requestService.get('/pages/category', (data)=> {
+    this.requestService.get('/pages/categories', (data)=> {
       this.categories = data.categories;
     }, null)
-    this.requestService.get('/pages/department', (data)=> {
+    this.requestService.get('/pages/departments', (data)=> {
       this.departments = data.departments;
     }, null)
+    this.requestService.get('/pages/tags/', (data) => this.allTags = data, null);
+    this.requestService.get('/search/all', (data) => {
+      this.allUsers = data.results.map((user)=> {
+        user.value = user.username;
+        user.display = user.full_name;
+        return user;
+      });
+    }, null);
+
   }
 
   ngOnInit() {
@@ -37,11 +51,17 @@ export class EditComponent implements OnInit {
 
   save(onSucessfulSave) {
     // Remove unwanted attributes from JSON.
-    let ignoredKeys = ["updated_at", "current", "created", "url"];
+    let ignoredKeys = ["updated_at", "current", "created", "url", "editors"];
     let filteredPage = Object.assign({}, this.page);
     for(let i in ignoredKeys){
       delete filteredPage[ignoredKeys[i]];
     }
+
+    //Add editors as an array of username strings.
+    filteredPage.editors = this.editors.map((user: any) => {
+      if (typeof(user) == "string") { return user }
+      return user.username;
+    })
 
     // Send New Page to server.
     this.requestService.post('/pages/admin/' + this.page.url, filteredPage, (data)=> {
