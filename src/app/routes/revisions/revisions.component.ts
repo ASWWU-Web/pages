114c4f37795d69ae21e97e266b9ceca772973b28
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-03-08 10:55:37 -0800

    Restore revision now works. Restore button disabled if current revision is selected.

--- src/app/routes/revisions/revisions.component.ts
+++ src/app/routes/revisions/revisions.component.ts
@@ -11,23 +11,39 @@ export class RevisionsComponent {
   revisions: any;
   pageURL: string;
   revisionURL: string;
+  current: boolean = true;
 
   constructor(private requestService: RequestService, private route: ActivatedRoute, private router: Router) {
+    // get page URL
     this.route.params.subscribe((params) => {
       this.pageURL = params.pageURL;
-      this.revisionURL = '/pages/' + this.pageURL;
+      this.loadRevision(null);
     });
+    this.getAllRevisions();
+  }
 
+  loadRevision(id) {
+    if (id == null) {
+      this.current = true;
+      this.revisionURL = '/pages/' + this.pageURL;
+    } else {
+      this.current = false;
+      this.revisionURL = '/pages/admin/' + this.pageURL + '/revision/' + id;
+    }
+  }
+
+  getAllRevisions() {
     this.requestService.get('/pages/admin/' + this.pageURL + '/revision', (data) => {
       this.revisions = data.results.reverse();
     }, null);
   }
 
-  loadRevision(id) {
-    this.revisionURL = '/pages/admin/' + this.pageURL + '/revision/' + id;
-  }
-
   restoreRevision() {
-    // TODO: post request for setting revision
+    this.requestService.post(this.revisionURL, {}, (data)=> {
+      console.log(data);
+      this.getAllRevisions();
+    }, (error) => {
+      alert(error.message);
+    });
   }
 }
