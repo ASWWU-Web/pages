073aef1890e51975e1e70ea57e8fbecc705050ff
Ryan Rabello <ryan.s.rabello@gmail.com>
2018-03-30 17:46:25 -0700

    Updated resolveCoverImage()

--- src/app/shared/resolveCoverImage.ts
+++ src/app/shared/resolveCoverImage.ts
@@ -3,7 +3,7 @@ import { MEDIA_URI } from '../config';
 
 export function resolveCoverImage(cover_image_URI:string, media_link:string = MEDIA_URI) {
   if (environment.production) {
-    return (MEDIA_URI + '/' + cover_image_URI);
+    return (media_link + '/' + cover_image_URI);
   } else {
     if(cover_image_URI){
       var image = cover_image_URI.replace("cms", "");
